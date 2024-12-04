2a3df5043bfd8270af738bf336ebc7be0150738d
igaramadana <igrmdns085@gmail.com>
2024-12-04 08:37:53 +0700

    rev connection

--- config/database.php
+++ config/database.php
@@ -1,17 +1,13 @@
 <?php
-// Konfigurasi koneksi database menggunakan driver sqlsrv
-
-$serverName = "pop-os"; // Ganti dengan nama server Anda
+$serverName = "pop-os"; 
 $connectionOptions = array(
-    "Database" => "DBEthicX", // Ganti dengan nama database Anda
-    "Uid" => "sa", // Ganti dengan username SQL Server Anda
-    "PWD" => "Igaramadana123#" // Ganti dengan password SQL Server Anda
+    "Database" => "DBEthicX",
+    "Uid" => "sa", 
+    "PWD" => "Igaramadana123#" 
 );
 
-// Membuat koneksi ke database menggunakan sqlsrv_connect
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-// Mengecek apakah koneksi berhasil
 if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true)); // Menampilkan error jika koneksi gagal
-}
+    die(print_r(sqlsrv_errors(), true));
+}   
