0cef5fce6dcb67cbc345d7eaf41a626c8d5c7828
igaramadana <igrmdns085@gmail.com>
2024-12-05 16:51:19 +0700

    update routing ke login.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php 
-header('pages/login.php')
-?>
+<?php
+session_start();
+
+// Memeriksa apakah pengguna sudah login, jika tidak, alihkan ke halaman login
+if (!isset($_SESSION['user_id'])) {
+    header('Location: pages/login.php');
+    exit();
+}
