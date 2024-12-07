77848f99d36a8549965b1fd3ee9cdd93265dfe59
igaramadana <igrmdns085@gmail.com>
2024-12-07 10:37:08 +0700

    fixing delete pelanggaran

--- process/admin/process_hapus_pelanggaran.php
+++ process/admin/process_hapus_pelanggaran.php
@@ -14,25 +14,17 @@ if (isset($_GET['id'])) {
 
     // Cek apakah penghapusan berhasil
     if ($stmt) {
-<<<<<<< HEAD
         // Redirect kembali ke halaman kelola tata tertib dengan status berhasil
-        header ("Location: /PWD_PBLSITatib/pages/admin/kelola_tatatertib.php?status=deleted");
-    } else {
-        // Jika gagal, redirect dengan status error
-        header ("Location: /PWD_PBLSITatib/pages/admin/kelola_tatatertib.php?status=error");
-    }
-} else {
-    // Jika ID tidak ditemukan, redirect ke halaman utama
-=======
-        // Redirect kembali ke halaman kelola pelanggaran dengan status berhasil
         header("Location: /PWD_PBLSITatib/pages/admin/kelola_tatatertib.php?status=deleted");
+        exit;
     } else {
         // Jika gagal, redirect dengan status error
         header("Location: /PWD_PBLSITatib/pages/admin/kelola_tatatertib.php?status=error");
+        exit;
     }
 } else {
     // Jika ID tidak ditemukan, redirect ke halaman kelola pelanggaran
->>>>>>> 3060951151108f4a1a1e19e7d0167a6be50a40e1
     header("Location: /PWD_PBLSITatib/pages/admin/kelola_tatatertib.php");
+    exit;
 }
 ?>
