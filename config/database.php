166f55f3ef385dca6322ecb1f585a1640ce08884
igaramadana <igrmdns085@gmail.com>
2024-12-09 12:09:07 +0700

    update oop

--- config/database.php
+++ config/database.php
@@ -1,13 +1,38 @@
 <?php
-$serverName = "pop-os"; 
-$connectionOptions = array(
-    "Database" => "DBEthicX",
-    "Uid" => "sa", 
-    "PWD" => "Igaramadana123#" 
-);
+class DatabaseConnection {
+    private $serverName = "pop-os"; 
+    private $connectionOptions;
+    private $conn;
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+    public function __construct() {
+        $this->connectionOptions = array(
+            "Database" => "DBEthicX",
+            "Uid" => "sa", 
+            "PWD" => "Igaramadana123#" 
+        );
+        $this->conn = $this->connect();
+    }
 
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
+    private function connect() {
+        $conn = sqlsrv_connect($this->serverName, $this->connectionOptions);
+        if ($conn === false) {
+            die(print_r(sqlsrv_errors(), true)); // Menampilkan error jika koneksi gagal
+        }
+        return $conn;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function __destruct() {
+        if ($this->conn) {
+            sqlsrv_close($this->conn); // Menutup koneksi setelah selesai
+        }
+    }
 }
+
+$db = new DatabaseConnection();
+$conn = $db->getConnection();
+?>
+s
