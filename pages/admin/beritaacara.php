8fd0328ac4ed20154d95108c91849ecc986edf23
igaramadana <igrmdns085@gmail.com>
2024-12-10 09:29:08 +0700

    update berita acara

---
+++ pages/admin/beritaacara.php
@@ -0,0 +1,43 @@
+<!--*******************
+Preloader start
+********************-->
+<div id="preloader">
+    <div class="lds-ripple">
+        <div></div>
+        <div></div>
+    </div>
+</div>
+<!--*******************
+Preloader end
+********************-->
+
+<div id="main-wrapper">
+    <?php
+    include("header.php");
+    include("sidebar.php")
+    ?>
+
+    <div class="conten-body">
+        <div class="container-fluid">
+            <div class="row page-titles">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active">
+                        <a href="javascript:void(0)">Form Berita Acara</a>
+                    </li>
+                </ol>
+            </div>
+            <div class="row gy-4">
+                <div class="col-xl-12 col-lg-12 col-xxl-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Form Berita Acara</h4>
+                        </div>
+                        <div class="card-body text-center">
+                            <h5>Download Berita Acara (PDF)</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
